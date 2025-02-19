7977a0b80083396d1d0710b9e8e50e906ac5ca3c
Denis Peralta <d3nisfp@gmail.com>
2025-02-19 11:54:03 -0300

    fixed dotenv and add loading

--- src/app/api/db.js
+++ src/app/api/db.js
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 // Create a single supabase client for interacting with your database
-const supabase = createClient("", process.env.NEXT_PUBLIC_ANON_KEY || "");
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_ANON_KEY || ""
+);
 
 const getTorneos = async () => {
   const { data, error } = await supabase
