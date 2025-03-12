8f0f538472f4c40c3e4b71485618d19b64a76385
Denis Peralta <d3nisfp@gmail.com>
2025-03-12 07:34:27 -0300

    fixing table layout

--- src/components/table.js
+++ src/components/table.js
@@ -1,7 +1,7 @@
 export default function Table({ children, columns }) {
   return (
     <div
-      className={`grid ${columns} w-full justify-center text-xl sm:w-3/4 sm:text-3xl md:max-w-screen-sm lg:max-w-screen-lg`}
+      className={`grid ${columns} w-full justify-center text-xl sm:text-3xl md:w-full md:max-w-screen-sm lg:max-w-screen-lg`}
     >
       {children}
     </div>
